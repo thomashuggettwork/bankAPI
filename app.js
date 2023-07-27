06e8a54bfac2ff68720ba53dccc9d8c8d378ec3d
thomashuggettwork <thomas.huggett@avanade.com>
2023-07-27 15:40:09 -0400

    gitignore

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req,res) => {
+    res.send("workplz");
+})
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+})
